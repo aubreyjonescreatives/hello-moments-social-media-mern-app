5ea2d50d10629bd38db330fc9db30d41d7344a43
Aubrey Jones <aubrey_jones07@hotmail.com>
2023-02-27 01:14:24 -0700

    added userSchema to user model

--- server/models/User.js
+++ server/models/User.js
@@ -22,6 +22,30 @@ const UserSchema = new mongoose.Schema(
             max: 50,
             unique: true
         }, 
+        password: {
+            type: String, 
+            required: true, 
+            min: 8, 
+           
+        }, 
+        picturePath: {
+           type: String, 
+           default: "", 
+           
+        }, 
+        friends: {
+            type: Array, 
+            default: []
+        }, 
+        location: String, 
+        occupation: String, 
+        viewProfile: Number, 
+        impressions: Number,
        
-    }
-)
+    }, 
+    { timestamps: true }
+); 
+
+const User = mongoose.model("User", UserSchema)
+
+export default User; 
