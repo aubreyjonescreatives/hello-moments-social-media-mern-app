ec3d2ca39d5707cdf1e96ece9be6aea710be91ea
Aubrey Jones <aubrey_jones07@hotmail.com>
2023-03-02 16:29:04 -0700

    added mui theme to client

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,15 @@ import NavBar from 'scenes/NavBar';
 import HomePage from 'scenes/HomePage'; 
 import LoginPage from 'scenes/LoginPage';
 import ProfilePage from 'scenes/ProfilePage';
+import { useMemo } from "react"; 
+import { useSelector } from 'react-redux'; 
+import {CssBaseline, ThemeProvider} from "@mui/material"; 
+import { createTheme } from "@mui/material/styles"; 
+import { themeSettings } from "./theme"; 
  
+
+
+
 
 const Layout = () => {
   return (
@@ -40,12 +48,16 @@ const router = createBrowserRouter([
 
 function App() {
 
- 
+ const mode = useSelector((state) => state.mode); 
+ const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]); 
 
 
   return (
    <>
+   <ThemeProvider theme={theme}>
+    <CssBaseline />
   <RouterProvider router={router}></RouterProvider>
+  </ThemeProvider>
    </>
   );
 }
