87df616a64a69fa429f3d3ab273ab0fa8a4235b0
Aubrey Jones <aubrey_jones07@hotmail.com>
2023-03-02 15:32:31 -0700

    added redux store for possible bugs

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,8 @@
 import { createBrowserRouter, RouterProvider, Outlet } from 'react-router-dom'; 
-import NavBar from 'scenes/NavBar/NavBar.jsx';
-import HomePage from 'scenes/HomePage/HomePage.jsx'; 
-import LoginPage from 'scenes/LoginPage/LoginPage.jsx';
-import ProfilePage from 'scenes/ProfilePage/ProfilePage.jsx';
+import NavBar from 'scenes/NavBar';
+import HomePage from 'scenes/HomePage'; 
+import LoginPage from 'scenes/LoginPage';
+import ProfilePage from 'scenes/ProfilePage';
  
 
 const Layout = () => {
