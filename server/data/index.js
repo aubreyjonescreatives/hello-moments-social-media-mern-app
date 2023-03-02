b5ef81f70be7f8e1214e59110882bd5f6fa71362
Aubrey Jones <aubrey_jones07@hotmail.com>
2023-03-02 02:16:31 -0700

    installed create-react-app for client folder

--- server/data/index.js
+++ server/data/index.js
@@ -66,3 +66,58 @@ export const users = [
         __v: 0, 
     }, 
 ]
+
+
+
+export const posts = [
+    {
+        _id: userIds[0], 
+        firstName: "test", 
+        lastName: "me", 
+        email: "aaaaaaa@gmail.com", 
+        password: "$2b$10$dsasdgsagasda//G9JxQ4bQ8KXf4OAIe/X/AK9skyWUy", 
+        picturePath: "p11.jpeg", 
+        friends: [], 
+        location: "San Fran, CA", 
+        occupation: "Software Engineer", 
+        viewedProfile: 14561, 
+        impressions: 888822, 
+        createdAt: 1115211422, 
+        updatedAt: 1115211422,
+        __v: 0,
+    }, 
+    {
+        _id: userIds[1], 
+        firstName: "Steve", 
+        lastName: "Ralph", 
+        email: "thataaa@gmail.com", 
+        password: "$!FEAS@!O)_IDJda//G9JxQ4bQ8KSf4OAIe/X/AK9skyWUy", 
+        picturePath: "p3.jpeg", 
+        friends: [], 
+        location: "New York, CA", 
+        occupation: "Degenerate", 
+        viewedProfile: 12351, 
+        impressions: 55555, 
+        createdAt: 1595589072, 
+        updatedAt: 1595589072,
+        __v: 0, 
+    }, 
+    {
+        _id: userIds[2], 
+        firstName: "Some", 
+        lastName: "Guy", 
+        email: "someguy@gmail.com", 
+        password: "da39a3ee5e6b4b0d3255bfef95601890afd80709", 
+        picturePath: "p4.jpeg", 
+        friends: [], 
+        location: "Canada, CA", 
+        occupation: "Data Scientist Hacker", 
+        viewedProfile: 45468, 
+        impressions: 19986, 
+        createdAt: 1288090662, 
+        updatedAt: 1288090662,
+        __v: 0, 
+    }, 
+]
+
+
--- server/index.js
+++ server/index.js
@@ -16,7 +16,8 @@ import { register } from "./controllers/auth.js";
 import { createPost } from "./controllers/posts.js"; 
 import { verifyToken } from "./middleware/auth.js"; 
 import User from "./models/User.js"; 
-
+import Post from "./models/Post.js";
+import { users, posts } from "./data/index.js"; 
 
 /* SECURITY WITH DOTENV */
 
@@ -78,5 +79,8 @@ mongoose.connect(process.env.MONGO_URL, {
     useUnifiedTopology: true, 
 }).then(() => {
     app.listen(PORT, () => console.log(`Server Started: ${PORT}`)); 
+   /* TESTING DATA */
+    User.insertMany(users); 
+    Post.insertMany(posts); 
 }).catch((error) => console.log(`${error} something went wrong.`)); 
 
