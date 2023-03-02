7c322b9541b348f4ae1ae6417a84a76bc10b02c3
Aubrey Jones <aubrey_jones07@hotmail.com>
2023-03-02 14:34:21 -0700

    added global state for lightmode and darkmode

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,51 @@
-import './App.css';
+import { createBrowserRouter, RouterProvider, Outlet } from 'react-router-dom'; 
+import NavBar from 'scenes/NavBar/NavBar.jsx';
+import HomePage from 'scenes/HomePage/HomePage.jsx'; 
+import LoginPage from 'scenes/LoginPage/LoginPage.jsx';
+import ProfilePage from 'scenes/ProfilePage/ProfilePage.jsx';
+ 
+
+const Layout = () => {
+  return (
+    <>
+    <NavBar />
+    <Outlet />
+
+    </>
+  )
+}
+
+
+const router = createBrowserRouter([
+  {
+    path: '/', 
+    element: <Layout />,
+
+    children: [
+      {
+        path: '/', 
+        element: <HomePage />
+      }, 
+      {
+        path: '/login', 
+        element: <LoginPage />
+      }, 
+      {
+        path: '/profile/:userId', 
+        element: <ProfilePage />
+      }, 
+    ]
+  }
+])
 
 function App() {
+
+ 
+
+
   return (
    <>
-   
+  <RouterProvider router={router}></RouterProvider>
    </>
   );
 }
