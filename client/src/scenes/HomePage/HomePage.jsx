c446680540997bdd8710b090fab0d7a7c8eb8eac
Aubrey Jones <aubrey_jones07@hotmail.com>
2023-03-03 18:40:35 -0700

    fixed extra navigation placement

--- client/src/scenes/HomePage/HomePage.jsx
+++ client/src/scenes/HomePage/HomePage.jsx
@@ -1,13 +1,11 @@
 import React from 'react'
 import { Box } from "@mui/material"; 
-import NavBar from "scenes/NavBar/NavBar.jsx"; 
 
 
 const HomePage = () => {
   return (
     <>
     <Box>
-    <NavBar />
     <div>HomePage</div>
     </Box>
     </>
