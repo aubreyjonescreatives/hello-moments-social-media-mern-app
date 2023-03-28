6f3f49b4cce564d15d57cca70a8d699d5338109f
Aubrey Jones <aubrey_jones07@hotmail.com>
2023-03-28 03:00:10 -0600

    removed last commit

--- server/index.js
+++ server/index.js
@@ -92,13 +92,13 @@ app.use("/posts", postRoutes);
 
 // connect server to client for production
 
-if (process.env.NODE_ENV === 'production') {
-	app.use(express.static('client/build'));
-}
+// if (process.env.NODE_ENV === 'production') {
+// 	app.use(express.static('client/build'));
+// }
 
-app.get('*', (req, res) => {
-	res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
-});
+// app.get('*', (req, res) => {
+// 	res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+// });
 
 
 
