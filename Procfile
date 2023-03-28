2e6f113b48c5f393cb3748f6b6cff4dd89770b7c
Aubrey Jones <aubrey_jones07@hotmail.com>
2023-03-28 16:58:18 -0600

    updated project file and folder org for better H

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web:node package.json
+web: node index.js
--- index.js
+++ index.js
@@ -92,13 +92,14 @@ app.use("/posts", postRoutes);
 
 // connect server to client for production
 
-// if (process.env.NODE_ENV === 'production') {
-// 	app.use(express.static('client/build'));
-// }
+if (process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'staging') {
+	app.use(express.static('client/build'));
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname + '/client/build/index.html'));
+    });
+}
 
-// app.get('*', (req, res) => {
-// 	res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
-// });
+
 
 
 
