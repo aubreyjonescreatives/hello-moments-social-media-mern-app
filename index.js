b0aed0d92285afbe7f4b738fae0c87df82e27673
Aubrey Jones <aubrey_jones07@hotmail.com>
2023-03-28 22:21:51 -0600

    added crossOriginEmbedderPolicy

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ const app = express();
 
 app.use(express.json()); 
 //app.use(helmet()); 
-app.use(helmet({ contentSecurityPolicy: false, crossOriginResourcePolicy: false })); 
+app.use(helmet({ contentSecurityPolicy: false, crossOriginResourcePolicy: false, crossOriginEmbedderPolicy: false })); 
 app.use(morgan("common")); 
 app.use(bodyParser.json({ limit: "30mb", extended: true })); 
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true}));  
